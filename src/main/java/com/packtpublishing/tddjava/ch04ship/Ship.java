b9c33d95a796056c9f213b4c253c79842c7b2581
jaler <jalerw@gmail.com>
2016-09-29 08:33:15 +0800

    implement requirement 1

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,21 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    Location currentLocation = null;
+
+    public Ship(Location location) {
+        currentLocation = location;
+    }
+
+    public Ship() {
+
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -1,10 +1,22 @@
 package com.packtpublishing.tddjava.ch04ship;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import static org.testng.Assert.*;
 
 @Test
 public class ShipSpec {
+    private Ship ship = null;
+    private Location location = null;
 
+    @BeforeMethod
+    public void beforeTest(){
+        location= new Location(new Point(0, 0), Direction.NORTH);
+        ship = new Ship(location);
+    }
+
+    public void WhenInstantiatedThenLocationIsSet(){
+        Assert.assertEquals(location, ship.getCurrentLocation());
+    }
 }
