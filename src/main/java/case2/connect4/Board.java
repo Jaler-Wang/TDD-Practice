5fbeff9016c3747fa0286b97e5836348cbf528f6
jaler <jalerw@gmail.com>
2016-10-15 12:54:49 +0800

    this is the implement for req 6,7,8. The win status

--- src/main/java/case2/connect4/Board.java
+++ src/main/java/case2/connect4/Board.java
@@ -15,13 +15,15 @@ public class Board {
     private int[] index = new int[COLUMN];
     private String currentPlayer = "R";
     private PrintStream printStream = null;
+    private String winner = "";
 
-    public Board(PrintStream printStream){
+    public Board(PrintStream printStream) {
         this.printStream = printStream;
-        for(int i = 0; i < COLUMN; i++){
+        for (int i = 0; i < COLUMN; i++) {
             index[i] = 0;
         }
     }
+
     public int getRow() {
         return ROW;
     }
@@ -33,19 +35,96 @@ public class Board {
     public int putDisc(int column) {
         checkColumn(column);
         boards[column][index[column]] = currentPlayer;
+        checkWinner(column, index[column]);
         switchPlayer();
         index[column]++;
         printBoard();
         return index[column] - 1;
     }
 
+    private void checkWinner(int column, int row) {
+        if (winner.equals("")) {
+            checkVerticalLine(column, row);
+        }
+        if (winner.equals("")) {
+            checkHorizontalLine(row);
+        }
+        if (winner.equals("")) {
+            checkDiagonal(column, row);
+        }
+    }
+
+    private void checkDiagonal(int column, int row) {
+        if (winner.equals("")) {
+            checkDiagonal(column, row, 1, -1);
+        }
+        if (winner.equals("")) {
+            checkDiagonal(column, row, -1, 1);
+        }
+        if (winner.equals("")) {
+            checkDiagonal(column, row, -1, -1);
+        }
+        if (winner.equals("")) {
+            checkDiagonal(column, row, 1, 1);
+        }
+    }
+
+    private void checkDiagonal(int column, int row, int cInc, int rInc) {
+        int continueNumber = 0;
+        while (column >= 0 && row >= 0 && column < COLUMN && row < ROW) {
+            if (currentPlayer.equals(boards[column][row])) {
+                continueNumber++;
+                setWinner(continueNumber);
+            } else {
+                continueNumber = 0;
+            }
+            column += cInc;
+            row += rInc;
+        }
+
+    }
+
+    private void checkHorizontalLine(int row) {
+        int continueNumber = 0;
+        for (int i = 0; i < COLUMN; i++) {
+            if (currentPlayer.equals(boards[i][row])) {
+                continueNumber++;
+                setWinner(continueNumber);
+            } else {
+                continueNumber = 0;
+            }
+        }
+    }
+
+    private void checkVerticalLine(int column, int row) {
+        if (row > 2) {
+            int continueNumber = 0;
+            while (row >= 0) {
+                if (boards[column][row].equals(currentPlayer)) {
+                    continueNumber++;
+                    setWinner(continueNumber);
+                } else {
+                    continueNumber = 0;
+                }
+                row--;
+            }
+        }
+    }
+
+    private void setWinner(int continueNumber) {
+        if (continueNumber > 3) {
+            winner = currentPlayer;
+        }
+    }
+
     private void printBoard() {
-        StringBuilder sb = new StringBuilder("|");
-        for(int i = 0; i < ROW; i++){
-            for(int j = 0; j < COLUMN; j++){
-                if(index[j] > i){
+        StringBuilder sb = new StringBuilder();
+        for (int i = ROW - 1; i >= 0; i--) {
+            sb.append("|");
+            for (int j = 0; j < COLUMN; j++) {
+                if (index[j] > i) {
                     sb.append(boards[j][i]);
-                }else{
+                } else {
                     sb.append(" ");
                 }
                 sb.append("|");
@@ -56,26 +135,26 @@ public class Board {
     }
 
     private void switchPlayer() {
-        if(currentPlayer.equals(RED)){
+        if (currentPlayer.equals(RED)) {
             currentPlayer = GREEN;
-        }
-        else{
+        } else {
             currentPlayer = RED;
         }
     }
 
-    private void checkColumn(int column){
-        if(column < 0 || column > COLUMN || index[column] == ROW){
+    private void checkColumn(int column) {
+        if (column < 0 || column > COLUMN || index[column] == ROW) {
             throw new RuntimeException("out of boarder");
         }
     }
+
     public String getDisc(int column) {
         return boards[column][0];
     }
 
     public int getDiscNumber() {
         int totalDiscs = 0;
-        for(int i = 0; i < COLUMN; i++){
+        for (int i = 0; i < COLUMN; i++) {
             totalDiscs += index[i];
         }
         return totalDiscs;
@@ -88,5 +167,9 @@ public class Board {
 
     public boolean isFinished() {
         return getDiscNumber() == COLUMN * ROW;
+    }
+
+    public String getWinner() {
+        return winner;
     }
 }
--- src/test/java/case2/connect4/BoardTest.java
+++ src/test/java/case2/connect4/BoardTest.java
@@ -21,19 +21,20 @@ public class BoardTest {
     public final ExpectedException exception = ExpectedException.none();
     private Board board = null;
     private OutputStream outputStream = null;
+
     @Before
-    public void init(){
+    public void init() {
         outputStream = new ByteArrayOutputStream();
         board = new Board(new PrintStream(outputStream));
     }
 
     @Test
-    public void whenTheGameIsStartedTheBoardIsEmpty(){
-       Assert.assertEquals(0, board.getDiscNumber());
+    public void whenTheGameIsStartedTheBoardIsEmpty() {
+        Assert.assertEquals(0, board.getDiscNumber());
     }
 
     @Test
-    public void whenDiscOutsideBoardThenRuntimeException(){
+    public void whenDiscOutsideBoardThenRuntimeException() {
         int column = -1;
         exception.expect(RuntimeException.class);
         exception.expectMessage("out of boarder");
@@ -41,14 +42,14 @@ public class BoardTest {
     }
 
     @Test
-    public void whenFirstDiscInsertedInColumnThenPositionIsZero(){
+    public void whenFirstDiscInsertedInColumnThenPositionIsZero() {
         int column = 1;
         int number = board.putDisc(column);
         Assert.assertEquals(0, number);
     }
 
     @Test
-    public void whenSecondDiscInsertedInColumnThenPositionIsOne(){
+    public void whenSecondDiscInsertedInColumnThenPositionIsOne() {
         int column = 1;
         board.putDisc(1);
         int number = board.putDisc(1);
@@ -56,17 +57,17 @@ public class BoardTest {
     }
 
     @Test
-    public void whenDiscInsertedThenNumberOfDiscIncrease(){
+    public void whenDiscInsertedThenNumberOfDiscIncrease() {
         int column = 1;
         board.putDisc(column);
         Assert.assertEquals(1, board.getDiscNumber());
     }
 
     @Test
-    public void whenPutDiscOnAFullColumnThenRuntimeException(){
+    public void whenPutDiscOnAFullColumnThenRuntimeException() {
         int column = 1;
         int maxDiscNumber = 6;
-        for(int i = 0; i < maxDiscNumber; i++){
+        for (int i = 0; i < maxDiscNumber; i++) {
             board.putDisc(column);
         }
         exception.expect(RuntimeException.class);
@@ -74,25 +75,25 @@ public class BoardTest {
     }
 
     @Test
-    public void whenFirstPlayerPlaysThenColorIsRed(){
+    public void whenFirstPlayerPlaysThenColorIsRed() {
         Assert.assertEquals("R", board.getCurrentPlayer());
     }
 
     @Test
-    public void whenSecondPlayerPlaysThenDiscColorIsGreen(){
+    public void whenSecondPlayerPlaysThenDiscColorIsGreen() {
         int column = 1;
         board.putDisc(column);
         Assert.assertEquals("G", board.getCurrentPlayer());
     }
 
     @Test
-    public void whenAskedForCurrentPlayerThenOutputNotice(){
+    public void whenAskedForCurrentPlayerThenOutputNotice() {
         board.getCurrentPlayer();
         Assert.assertEquals("Player R turn", outputStream.toString());
     }
 
     @Test
-    public void whenADiscIsIntroducedTheBoardIsPrinted(){
+    public void whenADiscIsIntroducedTheBoardIsPrinted() {
         int column = 1;
         board.putDisc(column);
 
@@ -100,34 +101,59 @@ public class BoardTest {
     }
 
     @Test
-    public void whenTheGameStartsItIsNotFinished(){
+    public void whenTheGameStartsItIsNotFinished() {
         Assert.assertFalse(board.isFinished());
     }
 
     @Test
-    public void whenTheBoardIsFullThenFinished(){
-        for(int i = 0; i < 7; i++){
-            for(int j = 0; j < 6; j++){
+    public void whenTheBoardIsFullThenFinished() {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 6; j++) {
                 board.putDisc(i);
             }
         }
         Assert.assertTrue(board.isFinished());
     }
 
+    @Test
+    public void when4VerticalDiscAreConnectedThenPlayerWins() {
+        int column1 = 1;
+        int column2 = 2;
+        for (int i = 0; i < 3; i++) {
+            board.putDisc(column1);
+            board.putDisc(column2);
+        }
+        board.putDisc(column1);
+        Assert.assertEquals("R", board.getWinner());
+    }
 
+    @Test
+    public void when4HorizontalDiscAreConnectedThenPlayerWins() {
+        for (int i = 0; i < 3; i++) {
+            board.putDisc(i);
+            board.putDisc(i);
+        }
+        board.putDisc(3);
+        Assert.assertEquals("R", board.getWinner());
+    }
 
+    @Test
+    public void whenTopLeftToBottomRightHave4DiscThenPlayerWins() {
+        int[] location = new int[]{6, 5, 5, 4, 4, 3, 4, 3, 3, 1, 3};
+        for (int i = 0; i < location.length; i++) {
+            board.putDisc(location[i]);
+        }
+        System.out.println(outputStream.toString());
+        Assert.assertEquals("R", board.getWinner());
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    @Test
+    public void whenBottomLeftToTopRightHave4DiscthenPlayerWins() {
+        int[] location = new int[]{0,1,1,2,3,2,2,3,4,3,3};
+        for (int i = 0; i < location.length; i++) {
+            board.putDisc(location[i]);
+        }
+        System.out.println(outputStream.toString());
+        Assert.assertEquals("R", board.getWinner());
+    }
 }
