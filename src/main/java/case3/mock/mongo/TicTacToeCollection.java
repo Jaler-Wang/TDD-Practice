e857da94ebb319a4e5ae640e38e58c58251eaece
jaler <jalerw@gmail.com>
2016-10-17 22:44:20 +0800

    In this requriment we learned when we hope there have a exception, we can use below method: Mockito.doThrow(Exception.class).when(MockObject).method();
    we implement save and drop method in the same way. so you can study the save then implement the drop by yourself

--- src/main/java/case3/mock/mongo/TicTacToeCollection.java
+++ src/main/java/case3/mock/mongo/TicTacToeCollection.java
@@ -3,6 +3,7 @@ package case3.mock.mongo;
 import case3.mock.bean.TicTacToeBean;
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
 
@@ -24,7 +25,22 @@ public class TicTacToeCollection {
     }
 
 
-    public void saveMove(TicTacToeBean ticTacToeBean) {
-        getMongoCollection().save(ticTacToeBean);
+    public boolean saveMove(TicTacToeBean ticTacToeBean) {
+
+        try {
+            getMongoCollection().save(ticTacToeBean);
+        } catch (MongoException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean drop() {
+        try {
+            getMongoCollection().drop();
+            return true;
+        } catch (MongoException e) {
+            return false;
+        }
     }
 }
--- src/test/java/case3/mock/mongo/TicTacToeCollectionSpec.java
+++ src/test/java/case3/mock/mongo/TicTacToeCollectionSpec.java
@@ -1,6 +1,7 @@
 package case3.mock.mongo;
 
 import case3.mock.bean.TicTacToeBean;
+import com.mongodb.MongoException;
 import org.jongo.MongoCollection;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,17 +10,20 @@ import org.testng.Assert;
 
 import java.net.UnknownHostException;
 
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by U0148394 on 10/16/2016.
  */
 public class TicTacToeCollectionSpec {
     private TicTacToeCollection ticTacToeCollection;
-
+    private TicTacToeBean ticTacToeBean;
+    private MongoCollection mongoCollection;
     @Before
     public void init() throws UnknownHostException {
         ticTacToeCollection = Mockito.spy(new TicTacToeCollection());
+        ticTacToeBean = new TicTacToeBean(3,2,1,"Y");
+        mongoCollection = Mockito.mock(MongoCollection.class);
     }
     @Test
     public void whenInstantiatedThenMongoHasDbNameTicTacToe() throws UnknownHostException {
@@ -33,10 +37,40 @@ public class TicTacToeCollectionSpec {
 
     @Test
     public void whenSaveMoveThenInvokeMongoCollectionSave(){
-        TicTacToeBean ticTacToeBean = new TicTacToeBean(3,2,1,"Y");
-        MongoCollection mongoCollection = Mockito.mock(MongoCollection.class);
         Mockito.doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
         ticTacToeCollection.saveMove(ticTacToeBean);
         Mockito.verify(mongoCollection, times(1)).save(ticTacToeBean);
+    }
+
+    @Test
+    public void whenSaveMoveThenReturnTrue(){
+        Mockito.doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
+        Assert.assertTrue(ticTacToeCollection.saveMove(ticTacToeBean));
+    }
+
+    @Test
+    public void givenExceptionWhenSaveMoveThenReturnFalse(){
+        Mockito.doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
+        doThrow(new MongoException("exception")).when(mongoCollection).save(Mockito.any(TicTacToeBean.class));
+        Assert.assertFalse(ticTacToeCollection.saveMove(ticTacToeBean));
+    }
+
+    @Test
+    public void whenDropThenInvokeMongoCollectionDrop(){
+        doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
+        ticTacToeCollection.drop();
+        verify(mongoCollection, timeout(1)).drop();
+    }
+
+    @Test
+    public void whenDropThenReturnTrue(){
+        doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
+        Assert.assertTrue(ticTacToeCollection.drop());
+    }
+    @Test
+    public void givenExceptionWhenDropThenReturnFalse(){
+        Mockito.doReturn(mongoCollection).when(ticTacToeCollection).getMongoCollection();
+        doThrow(MongoException.class).when(mongoCollection).drop();
+        Assert.assertFalse(ticTacToeCollection.drop());
     }
 }
